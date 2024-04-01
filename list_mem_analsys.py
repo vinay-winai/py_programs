cd928bf736d08c12a2f2d9d69b79f2c3e8088880
vinay-winai <vinai2k20@gmail.com>
2024-04-01 22:51:19 +0530

    improvements

--- list_mem_analsys.py
+++ list_mem_analsys.py
@@ -1,4 +1,5 @@
-arr = list(range(10000))
+# checking number of list contiguous memory discoutinuations and max chunk size
+arr = list(range(1,1_000_001))
 count = 0
 mx = 0
 prev=0
@@ -6,16 +7,18 @@ for idx,i in enumerate(arr[1:]):
     if id(i)-id(arr[idx])!=32:
         count+=1
         cidx = idx+1
-        print(idx,id(i))
+        # print(idx,id(i))
         mx = max(mx, cidx-prev)
         prev = cidx
 print(count,mx)
 
+# proving numpy stores values rather than references
 import numpy as np
 import sys
-arr = np.array(list(range(10000000)), dtype=np.int32)
+arr = np.array(list(range(1_000_000)), dtype=np.int32)
 print(len(arr) == (sys.getsizeof(arr)-112)//4)
 
+# showing lists store obj references and dynamic capacity increase
 s = 64
 arr = list(range(64))
 while s<165:
