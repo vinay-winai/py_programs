073f7a47ad4501636c74e577e3092b87e35166f4
vinay-winai <vinai2k20@gmail.com>
2024-04-25 13:51:04 +0530

    mods

--- list_mem_analsys.py
+++ list_mem_analsys.py
@@ -1,30 +1,35 @@
 # checking number of list contiguous memory discoutinuations and max chunk size
 import array
-arr = list(range(1,1_000_001))
-arr = array.array('L', arr)
-count = 0
-mx = 0
-prev=0
-for idx,i in enumerate(arr[1:]):
-    if id(i)-id(arr[idx])!=32:
-        count+=1
-        cidx = idx+1
-        # print(idx,id(i))
-        mx = max(mx, cidx-prev)
-        prev = cidx
-print(count,mx)
+from itertools import tee
+from functools import reduce
+import numpy as np
 
-# proving numpy stores values rather than references
-# import numpy as np
-# import sys
-# arr = np.array(list(range(1_000_000)), dtype=np.int32)
-# print(len(arr) == (sys.getsizeof(arr)-112)//4)
+# arr = list(range(1, 1_000_001))
+# arr = np.arange(1, 1_000_001)
+arr = array.array('L', list(range(1,1_000_001)))
+ids = [id(i) for i in arr]
 
-# # showing lists store obj references and dynamic capacity increase
-# s = 64
-# arr = list(range(64))
-# while s<165:
-#     s+=1
-#     arr.append(s)
-#     print(sys.getsizeof(arr)-56)
-#     print(len(arr) == (sys.getsizeof(arr)-56)//8)
+discons = []
+def make_contiguous_test():
+    curr_chunk = 0
+
+    def inner(acc, pair):
+        nonlocal curr_chunk
+        a, b = pair
+        diff = b - a
+        if diff == 32:
+            curr_chunk+=1
+            return acc + 1
+        discons.append(curr_chunk)
+        curr_chunk = 0
+        return acc
+    return inner
+
+continuity_test = make_contiguous_test()
+
+iter1, iter2 = tee(ids)
+next(iter2, None)
+pairs = zip(iter1, iter2)
+result = reduce(continuity_test, pairs, 0)
+print(f"Number of pairs with a difference of 32: {result}")
+print(sorted(discons)[:20])
