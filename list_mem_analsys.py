6c36ccbdfc35f76ecb1f9d12a33620de54e6ccc5
vinay-winai <vinai2k20@gmail.com>
2024-04-25 10:33:45 +0530

    new cases

--- list_mem_analsys.py
+++ list_mem_analsys.py
@@ -1,5 +1,7 @@
 # checking number of list contiguous memory discoutinuations and max chunk size
+import array
 arr = list(range(1,1_000_001))
+arr = array.array('L', arr)
 count = 0
 mx = 0
 prev=0
@@ -13,16 +15,16 @@ for idx,i in enumerate(arr[1:]):
 print(count,mx)
 
 # proving numpy stores values rather than references
-import numpy as np
-import sys
-arr = np.array(list(range(1_000_000)), dtype=np.int32)
-print(len(arr) == (sys.getsizeof(arr)-112)//4)
+# import numpy as np
+# import sys
+# arr = np.array(list(range(1_000_000)), dtype=np.int32)
+# print(len(arr) == (sys.getsizeof(arr)-112)//4)
 
-# showing lists store obj references and dynamic capacity increase
-s = 64
-arr = list(range(64))
-while s<165:
-    s+=1
-    arr.append(s)
-    print(sys.getsizeof(arr)-56)
-    print(len(arr) == (sys.getsizeof(arr)-56)//8)
+# # showing lists store obj references and dynamic capacity increase
+# s = 64
+# arr = list(range(64))
+# while s<165:
+#     s+=1
+#     arr.append(s)
+#     print(sys.getsizeof(arr)-56)
+#     print(len(arr) == (sys.getsizeof(arr)-56)//8)
--- listvsdict.py
+++ listvsdict.py
@@ -10,25 +10,26 @@ def lseek(ls):
         if i == 8889999:
             print(i)
             break
-
+@njit
 def dseek(ds):
     for k in ds:
         if k == 8889999:
             print(k)
             break
-
+@njit
+def dcreate():
+    return {i:i for i in range(10_000_000)}
 
 if __name__ == '__main__':
     with cProfile.Profile() as pr:
-        my_dictionary = {i:i for i in range(10_000_000)}
-        # o_d = OrderedDict({i:i for i in range(10_000_000)})
-        my_list = np.arange(10_000_000)
-        # my_list = [i for i in range(100_000_000)]
-        
-        for i in range(2):
-            lseek(my_list)
-            dseek(my_dictionary)
-            # dseek(o_d)
+        # my_list = np.arange(10_000_000)
+        # od = OrderedDict({i:i for i in range(10_000_000)})
+        ds = dcreate()
+
+        for i in range(10):
+            # lseek(my_list)
+            dseek(ds)
+            # dseek(od)
     results = pstats.Stats(pr)
     results.sort_stats(pstats.SortKey.TIME)
     results.print_stats(10)
